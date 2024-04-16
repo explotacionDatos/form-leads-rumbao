99a3be1cb378eeb1e5585b2b06a4655f03f36ebb
Gonzalo Vazquez <92376688+gonemvaz012@users.noreply.github.com>
2024-04-16 12:30:33 -0300

    commit: paginación de formulario de rumbao , postventa y bdc (#24)
    Co-authored-by: Gvazquez <vazquez.gonzaloemanuel@gmail.com>

--- src/Pages/FormRumbaoPost.jsx
+++ src/Pages/FormRumbaoPost.jsx
@@ -4,8 +4,8 @@ import Form from "../components/Form";
 const FormRumbao = () => {
   return (
     <div className="page-ctnr">
-      <h1 className="page-ctnr__title">GRUPO PEREZ RUMBAO - BDC</h1>
-      <Form />
+      <h1 className="page-ctnr__title">GRUPO PEREZ RUMBAO - POSTVENTA</h1>
+      <Form taller={"post"} />
     </div>
   );
 };
